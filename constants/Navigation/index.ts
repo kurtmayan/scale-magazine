20b0d82451ac61e5afb31bc38d54429c81413bb5
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-12-04 13:48:54 +0800

    navigation bar
    Created a responsive navigation bar

---
+++ constants/Navigation/index.ts
@@ -0,0 +1,31 @@
+export type NavLinkType = {
+  url: string;
+  label: string;
+};
+
+export const navLink: NavLinkType[] = [
+  {
+    url: "/",
+    label: "home",
+  },
+  {
+    url: "/about-us",
+    label: "about us",
+  },
+  {
+    url: "/awards",
+    label: "awards",
+  },
+  {
+    url: "/startups",
+    label: "startups",
+  },
+  {
+    url: "/industries",
+    label: "industries",
+  },
+  {
+    url: "/more",
+    label: "more",
+  },
+];
---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export { navLink } from "./Navigation";
