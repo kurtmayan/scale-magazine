2fef973ede0d5f0d895ba79f05851cd89f21d0ab
kurtmayan <johnkurt@mayan.com.ph>
2025-12-26 15:49:17 +0800

    feat: added more mobile sidebar

--- components/custom/Sidebar/MobileSidebarTrigger/index.tsx
+++ components/custom/Sidebar/MobileSidebarTrigger/index.tsx
@@ -24,6 +24,7 @@ import { Category } from "@/sanity.types";
 export function MobileSidebar(props: { categories: Array<Category> }) {
   const pathname = usePathname();
   const isIndustriesRoute = pathname.startsWith("/blog/");
+  const isMoreRoute = pathname.startsWith("/more");
 
   return (
     <Sheet>
@@ -44,7 +45,7 @@ export function MobileSidebar(props: { categories: Array<Category> }) {
           <SheetDescription />
           <div className="grid gap-5">
             {headerNavLink.map((nav, index) => {
-              if (nav.hasChild) {
+              if (nav.hasChild && nav.label === "industries") {
                 return (
                   <Accordion
                     key={index}
@@ -82,6 +83,51 @@ export function MobileSidebar(props: { categories: Array<Category> }) {
                             `}
                           >
                             <Link href={"/blog/" + e.tag}>{e.tag}</Link>
+                          </SheetClose>
+                        ))}
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                );
+              }
+              if (nav.hasChild && nav.label === "more") {
+                return (
+                  <Accordion
+                    key={index}
+                    type="single"
+                    collapsible
+                    defaultValue={isMoreRoute ? "more" : undefined}
+                    className="w-full rounded-none"
+                  >
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger
+                        className={`
+                          py-2 px-5 uppercase font-semibold text-2xl font-alumni-sans rounded-none justify-between items-center
+                          ${isMoreRoute ? "bg-neutral-900 text-white" : "bg-white text-black"}
+                        `}
+                      >
+                        {nav.label}
+                      </AccordionTrigger>
+                      <AccordionContent
+                        className={`flex flex-col ${
+                          isMoreRoute ? "bg-neutral-900" : "bg-white"
+                        }`}
+                      >
+                        {nav.child?.map((e, index) => (
+                          <SheetClose
+                            key={index}
+                            className={`
+                              py-2 px-6 uppercase font-semibold text-2xl font-alumni-sans text-left 
+                              ${
+                                isMoreRoute
+                                  ? pathname === e.label
+                                    ? "bg-[#1E1E1E] text-white"
+                                    : "text-neutral-300 hover:bg-[#1E1E1E] hover:text-white "
+                                  : "text-black"
+                              }
+                            `}
+                          >
+                            <Link href={e.url}>{e.label}</Link>
                           </SheetClose>
                         ))}
                       </AccordionContent>
