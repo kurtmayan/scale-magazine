068d13d3ec00b70ece9a2e2d3a1d3b1c9a62a0ca
kurtmayan <johnkurt@mayan.com.ph>
2025-12-29 14:13:35 +0800

    feat: added award page

---
+++ app/(pages)/awards/page.tsx
@@ -0,0 +1,45 @@
+import { Inter, TiroDevanagariSanskrit } from "@/components/custom/Typography";
+
+export default function Awards() {
+  return (
+    <div>
+      <div className="bg-linear-to-b from-gray-200 via-gray-200 to-white py-14">
+        <TiroDevanagariSanskrit className="max-sm:text-xl max-sm:w-93.25 text-center mx-auto sm:text-[35px]">
+          Awards & Recognition
+        </TiroDevanagariSanskrit>
+        <TiroDevanagariSanskrit className="max-sm:text-sm max-sm:w-93.25 text-center mx-auto sm:text-2xl">
+          Through the power of media and technology, SCALE celebrates the
+          leaders shaping the country’s economic landscape.
+        </TiroDevanagariSanskrit>
+      </div>
+      <div className="mt-10 max-sm:w-93 mx-auto sm:w-11/12">
+        <div>
+          <TiroDevanagariSanskrit className="max-sm:text-base sm:text-[26px]">
+            Top 100 Real Estate Brokers in the Philippines
+          </TiroDevanagariSanskrit>
+          <Inter className="max-sm:text-xs italic sm:text-xl">
+            Coming soon – A data-informed, editorially curated list recognizing
+            excellence and integrity in real estate practice.
+          </Inter>
+        </div>
+
+        <div className="mt-6">
+          <TiroDevanagariSanskrit className="max-sm:text-base sm:text-[26px]">
+            Top 100 Real Estate Brokers in the Philippines
+          </TiroDevanagariSanskrit>
+          <Inter className="max-sm:text-xs italic sm:text-xl">
+            Coming soon – A data-informed, editorially curated list recognizing
+            excellence and integrity in real estate practice.
+          </Inter>
+        </div>
+      </div>
+
+      <div className="my-16">
+        <TiroDevanagariSanskrit className="max-sm:text-lg sm:text-[28px] mx-auto max-sm:w-88.5 sm:w-178.75 text-center">
+          Criteria and nomination details will be released soon, supported by
+          SCALE’s media-tech evaluation framework.
+        </TiroDevanagariSanskrit>
+      </div>
+    </div>
+  );
+}
