8af655087ca5120a1a0f6c617fa0011fc8e95e5a
ItsYahBoiLaurence <laurence@mayan.com.ph>
2026-01-22 11:35:31 +0800

    Clickable articles in homepage

--- components/custom/Banner/CategoryBanner/index.tsx
+++ components/custom/Banner/CategoryBanner/index.tsx
@@ -16,10 +16,14 @@ export default function CategoryBanner({
 }) {
   if (!blogs || blogs.length === 0) return null;
   const [primaryBlog, ...restBlogs] = blogs;
-  const rightSectionBlog = blogs.slice(1, 3);
-  const leftSectionFirstBlog = blogs[3];
-  const leftSectionLastBlog = blogs[4];
+  const leftSectionBlog = blogs.slice(1, 3);
+  const rightSectionFirstBlog = blogs[3];
+  const rightSectionLastBlog = blogs[4];
   const remainingBlogs = blogs.slice(1, 6);
+
+  console.log(
+    `/blog/${category.toLowerCase()}/${primaryBlog.slug.current}}/${primaryBlog.slug.current}`,
+  );
 
   return (
     primaryBlog && (
@@ -31,7 +35,10 @@ export default function CategoryBanner({
         {/* Desktop */}
         <div className="hidden xl:grid grid-cols-2 gap-20">
           <div className="flex flex-col gap-22.5">
-            <div className="relative aspect-video -z-10 w-179 h-214.75">
+            <Link
+              href={`/blog/${category.toLowerCase()}/${primaryBlog.slug.current}}/${primaryBlog.slug.current}`}
+              className="hover:cursor-pointer relative aspect-video -z-10 w-179 h-214.75  "
+            >
               <Image
                 src={urlFor(primaryBlog.featuredImage).url()}
                 alt={primaryBlog.title}
@@ -39,33 +46,41 @@ export default function CategoryBanner({
                 priority
                 className="object-cover"
               />
-            </div>
+            </Link>
             <div className="grid grid-cols-2 gap-22.5">
-              {rightSectionBlog &&
-                rightSectionBlog.map(({ title, slug, featuredImage }) => (
-                  <div key={slug.current} className="flex flex-col gap-3">
-                    <div className="relative aspect-square -z-10 w-full h-72.5">
-                      <Image
-                        src={urlFor(featuredImage).url()}
-                        alt={slug.current}
-                        fill
-                        priority
-                        className="object-cover"
-                      />
+              {leftSectionBlog &&
+                leftSectionBlog.map(({ title, slug, featuredImage }) => (
+                  <Link
+                    href={`/blog/${category.toLowerCase()}/${slug.current}`}
+                    key={slug.current}
+                  >
+                    <div className="flex flex-col gap-3">
+                      <div className="relative aspect-square -z-10 w-full h-72.5">
+                        <Image
+                          src={urlFor(featuredImage).url()}
+                          alt={slug.current}
+                          fill
+                          priority
+                          className="object-cover"
+                        />
+                      </div>
+                      <AlumniSans className="uppercase text-2xl">
+                        {category}
+                      </AlumniSans>
+                      <TimesNewRoman className="uppercase text-2xl">
+                        {title}
+                      </TimesNewRoman>
                     </div>
-                    <AlumniSans className="uppercase text-2xl">
-                      {category}
-                    </AlumniSans>
-                    <TimesNewRoman className="uppercase text-2xl">
-                      {title}
-                    </TimesNewRoman>
-                  </div>
+                  </Link>
                 ))}
             </div>
           </div>
 
           <div className="flex flex-col gap-20">
-            <div className="grid gap-5">
+            <Link
+              href={`/blog/${category.toLowerCase()}/${primaryBlog.slug.current}`}
+              className="grid gap-5"
+            >
               <AlumniSans className="uppercase text-[28px]">
                 {category}
               </AlumniSans>
@@ -75,98 +90,112 @@ export default function CategoryBanner({
               <Inter className="text-[22px]">
                 {primaryBlog.shortDescription}
               </Inter>
-            </div>
-            {leftSectionFirstBlog && (
+            </Link>
+
+            {rightSectionFirstBlog && (
               <div className="flex flex-col gap-20">
-                <div className="flex flex-row gap-5 items-center">
-                  <div className="aspect-square w-68.5 h-64.5 relative">
+                <Link
+                  href={`/blog/${category.toLowerCase()}/${rightSectionFirstBlog.slug.current}`}
+                >
+                  <div className="flex flex-row gap-5 items-center">
+                    <div className="aspect-square w-68.5 h-64.5 relative">
+                      <Image
+                        src={urlFor(rightSectionFirstBlog.featuredImage).url()}
+                        alt={rightSectionFirstBlog.title}
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+                    <div className="flex flex-col ">
+                      <AlumniSans className="uppercase text-2xl">
+                        {category}
+                      </AlumniSans>
+                      <TimesNewRoman className="text-[32px]">
+                        {rightSectionFirstBlog.title}
+                      </TimesNewRoman>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            )}
+
+            {rightSectionLastBlog && (
+              <Link
+                href={`blog/${category.toLowerCase()}/${rightSectionLastBlog.slug.current}`}
+              >
+                <div className="flex flex-col gap-5">
+                  <div className="w-170.75 h-155 aspect-square relative">
                     <Image
-                      src={urlFor(leftSectionFirstBlog.featuredImage).url()}
-                      alt={leftSectionFirstBlog.title}
+                      src={urlFor(rightSectionLastBlog.featuredImage).url()}
+                      alt={rightSectionLastBlog.slug.current}
                       fill
                       className="object-cover"
                     />
                   </div>
-                  <div className="flex flex-col ">
-                    <AlumniSans className="uppercase text-2xl">
+                  <div className="flex flex-col gap-5">
+                    <AlumniSans className="uppercase text-[28px]">
                       {category}
                     </AlumniSans>
-                    <TimesNewRoman className="text-[32px]">
-                      {leftSectionFirstBlog.title}
+                    <TimesNewRoman className="text-4xl">
+                      {rightSectionLastBlog.title}
                     </TimesNewRoman>
                   </div>
                 </div>
-              </div>
-            )}
-
-            {leftSectionLastBlog && (
-              <div className="flex flex-col gap-5">
-                <div className="w-170.75 h-155 aspect-square relative">
-                  <Image
-                    src={urlFor(leftSectionLastBlog.featuredImage).url()}
-                    alt={leftSectionLastBlog.slug.current}
-                    fill
-                    className="object-cover"
-                  />
-                </div>
-                <div className="flex flex-col gap-5">
-                  <AlumniSans className="uppercase text-[28px]">
-                    {category}
-                  </AlumniSans>
-                  <TimesNewRoman className="text-4xl">
-                    {leftSectionLastBlog.title}
-                  </TimesNewRoman>
-                </div>
-              </div>
+              </Link>
             )}
           </div>
         </div>
 
         {/* Mobile */}
         <div className="xl:hidden">
-          <div className="relative aspect-square w-full">
-            <Image
-              src={urlFor(primaryBlog.featuredImage).url()}
-              alt={primaryBlog.title}
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div className="grid gap-1.25">
-            <AlumniSans className="text-[18px] uppercase">
-              {category}
-            </AlumniSans>
-            <TimesNewRoman className="text-xl">
-              {primaryBlog.title}
-            </TimesNewRoman>
-            <Inter className="text-xs font-light">
-              {primaryBlog.shortDescription}
-            </Inter>
-          </div>
+          <Link href={`/blog/${category}/${primaryBlog.slug.current}`}>
+            <div className="relative aspect-square w-full">
+              <Image
+                src={urlFor(primaryBlog.featuredImage).url()}
+                alt={primaryBlog.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="grid gap-1.25">
+              <AlumniSans className="text-[18px] uppercase">
+                {category}
+              </AlumniSans>
+              <TimesNewRoman className="text-xl">
+                {primaryBlog.title}
+              </TimesNewRoman>
+              <Inter className="text-xs font-light">
+                {primaryBlog.shortDescription}
+              </Inter>
+            </div>
+          </Link>
+
           <div className="grid gap-5 my-5">
             {remainingBlogs.length >= 1 &&
               remainingBlogs.map(({ featuredImage, slug, title }) => (
-                <div
-                  className="w-87.25 flex flex-cols gap-3"
+                <Link
+                  href={`blog/${category.toLowerCase()}/${slug.current}`}
                   key={slug.current}
                 >
-                  <div className="w-37.5 h-30 relative aspect-video">
-                    <Image
-                      src={urlFor(featuredImage).url()}
-                      alt={title}
-                      fill
-                      className="object-cover"
-                    />
+                  <div className="w-87.25 flex flex-cols gap-3">
+                    <div className="w-37.5 h-30 relative aspect-video">
+                      <Image
+                        src={urlFor(featuredImage).url()}
+                        alt={title}
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+                    <div>
+                      <AlumniSans className="text-sm uppercase">
+                        {category}
+                      </AlumniSans>
+                      <TimesNewRoman className="text-[16px]">
+                        {title}
+                      </TimesNewRoman>
+                    </div>
                   </div>
-                  <div>
-                    <AlumniSans className="text-sm uppercase">
-                      {category}
-                    </AlumniSans>
-                    <TimesNewRoman className="text-[16px]">
-                      {title}
-                    </TimesNewRoman>
-                  </div>
-                </div>
+                </Link>
               ))}
           </div>
           <Separator className="bg-black" />
